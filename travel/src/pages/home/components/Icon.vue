fadc6e0aa83f9420550c8704365be58c5e6e2505
wangchao <wanghao@learnmyself.com>
2021-05-17 12:31:06 +0800

    icons-swiper

---
+++ travel/src/pages/home/components/Icon.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="icon">
+    <swiper>
+      <swiper-slide
+        v-for="(page,index) of pages"
+        :key="index"
+      >
+        <div
+          class="icon-item"
+          v-for="item of page"
+          :key="item.id"
+        >
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="title">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcon',
+  data () {
+    return {
+      iconList: [{
+        id: '01',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+        desc: '故宫'
+      }, {
+        id: '02',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/cd9c5d035aa48862c3695becc06764e6.png',
+        desc: '八达岭长城'
+      }, {
+        id: '03',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/feac0f0a895cbc7c49920238529ca6e7.png',
+        desc: '文化古迹'
+      }, {
+        id: '04',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/5ffcd4a916f764f678263b27c1e2e00c.png',
+        desc: '天坛公园'
+      }, {
+        id: '05',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+        desc: '故宫'
+      }, {
+        id: '06',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/cd9c5d035aa48862c3695becc06764e6.png',
+        desc: '八达岭长城'
+      }, {
+        id: '07',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/feac0f0a895cbc7c49920238529ca6e7.png',
+        desc: '文化古迹'
+      }, {
+        id: '08',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/5ffcd4a916f764f678263b27c1e2e00c.png',
+        desc: '天坛公园'
+      }, {
+        id: '09',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/5ffcd4a916f764f678263b27c1e2e00c.png',
+        desc: '天坛公园2'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl';
+  @import '~styles/mixins.styl';
+  .icon >>> .swiper-container
+    height 0
+    padding-bottom 50%
+  .icon-item
+    position relative
+    float left
+    width 25%
+    height 0
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .title
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .44rem
+      line-height .44rem
+      text-align center
+      color $darkTextColor
+      ellipsis()
+</style>
