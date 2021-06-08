c4514c7f58403943bb5ccbbac791db649aec7e5e
wangchao <wanghao@learnmyself.com>
2021-06-08 16:39:24 +0800

    city-list better-scroll city-alphabet

---
+++ travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Alphabet">
+    <div class="item">A</div>
+    <div class="item">B</div>
+    <div class="item">C</div>
+    <div class="item">D</div>
+    <div class="item">E</div>
+    <div class="item">F</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .Alphabet
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    .item
+      height 0.4rem
+      text-align center
+      color $bgcolor
+</style>
---
+++ travel/src/pages/city/components/List.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+          <div class="item border-bottom">阿尔山</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    overflow hidden
+    .title
+      color #666
+      line-height .54rem
+      padding-left .2rem
+      background-color #eee
+    .button-list
+      overflow hidden
+      padding .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          text-align center
+          padding .1rem 0
+          border .02rem solid #ccc
+          border-radius .06rem
+          margin .1rem
+    .item-list
+      .item
+        padding-left .2rem
+        line-height 0.72rem
+</style>
