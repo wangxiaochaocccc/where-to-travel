1469deda372439f30561757342810193de96303d
wangchao <wanghao@learnmyself.com>
2021-06-10 18:30:21 +0800

    城市列表搜索逻辑

--- travel/src/pages/city/components/Search.vue
+++ travel/src/pages/city/components/Search.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="请输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input type="text" v-model="keyWords" class="search-input" placeholder="请输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyWords">
+      <ul>
+        <li class="search-item border-bottom" v-for="(item) of list" :key="item.id">{{item.name}}</li>
+        <li v-show="hasNoData">没有找到匹配的数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyWords: '',
+      timer: null,
+      list: []
+    }
+  },
+  watch: {
+    keyWords () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyWords) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        let result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (value.spell.indexOf(this.keyWords) > -1 || value.name.indexOf(this.keyWords) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  }
 }
 </script>
 
@@ -24,4 +73,17 @@ export default {
       color #999
       border-radius .06rem
       padding 0 .1rem
+  .search-content
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background-color #eee
+    z-index 5
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      background-color #fff
 </style>
