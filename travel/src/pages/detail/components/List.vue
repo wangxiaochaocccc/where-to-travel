6501d0425da866cf491461e97e156626819fa3ba
wangchao <wanghao@learnmyself.com>
2021-07-12 16:57:54 +0800

    详情页列表-递归组件

---
+++ travel/src/pages/detail/components/List.vue
@@ -0,0 +1,31 @@
+<!--  -->
+<template>
+  <div>
+    <div class="item" v-for="(item, index) of list" :key="index">
+      <div class="item-title border-bottom">
+        {{item.title}}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+<style scoped lang="stylus">
+.item-title
+  height .8rem
+  line-height 0.8rem
+  padding 0 .2rem
+  font-size .32rem
+.item-children
+  padding 0 .2rem
+</style>
